99590c3e49cca4b42d89d4e226a368bc472f1a91
Dung Hoang <dunghq@sparkminds.net>
2026-01-19 17:55:30 +0700

    update port

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server đang chạy trên port ${PORT}`);
 });
