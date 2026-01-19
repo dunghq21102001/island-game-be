4441859d19f4351bdb794a82674f815cb16de866
Dung Hoang <dunghq@sparkminds.net>
2026-01-19 17:46:32 +0700

    update

--- index.js
+++ index.js
@@ -5,18 +5,11 @@ const connectDB = require("./config/database.js");
 const authRoutes = require("./routes/auth.routes.js");
 const userRoutes = require("./routes/user.routes.js");
 
-
 const app = express();
-
-connectDB();
-app.use(express.json());
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://coral-island-game.netlify.app"
-    ],
+    origin: ["http://localhost:5173", "https://coral-island-game.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
@@ -25,12 +18,14 @@ app.use(
 
 app.options("*", cors());
 
+app.use(express.json());
+
+connectDB();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server đang chạy trên port ${PORT}`);
 });
