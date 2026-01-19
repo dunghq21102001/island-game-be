4204a9ff5d6d9d08c590c5850ee899f91a4a8bfd
Dung Hoang <dunghq@sparkminds.net>
2026-01-19 17:33:56 +0700

    update cors

--- index.js
+++ index.js
@@ -10,7 +10,20 @@ const app = express();
 
 connectDB();
 app.use(express.json());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://coral-island-game.netlify.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+app.options("*", cors());
 
 
 app.use("/api/auth", authRoutes);
